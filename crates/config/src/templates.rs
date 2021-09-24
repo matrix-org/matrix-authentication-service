a1c7b7f82d1454a081605e1b334ca1ee06859928
Quentin Gliech <quenting@element.io>
2021-09-24 09:20:22 +0200

    Add missing file forgotten in ff41ae57624837240805f6492d78d5b6d3e002a3

---
+++ crates/config/src/templates.rs
@@ -0,0 +1,58 @@
+// Copyright 2021 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use async_trait::async_trait;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use super::ConfigurationSection;
+
+fn default_builtin() -> bool {
+    true
+}
+
+#[derive(Debug, Serialize, Deserialize, JsonSchema)]
+pub struct TemplatesConfig {
+    /// Path to the folder that holds the custom templates
+    #[serde(default)]
+    pub path: Option<String>,
+
+    /// Load the templates embedded in the binary
+    #[serde(default = "default_builtin")]
+    pub builtin: bool,
+}
+
+impl Default for TemplatesConfig {
+    fn default() -> Self {
+        Self {
+            path: None,
+            builtin: default_builtin(),
+        }
+    }
+}
+
+#[async_trait]
+impl ConfigurationSection<'_> for TemplatesConfig {
+    fn path() -> &'static str {
+        "templates"
+    }
+
+    async fn generate() -> anyhow::Result<Self> {
+        Ok(Self::default())
+    }
+
+    fn test() -> Self {
+        Self::default()
+    }
+}
