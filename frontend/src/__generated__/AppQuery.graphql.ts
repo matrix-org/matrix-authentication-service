815afb4535ec07abb6d177c408d53de6501ee284
Quentin Gliech <quenting@element.io>
2022-11-09 18:59:39 +0100

    Bootstrap the frontend

---
+++ frontend/src/__generated__/AppQuery.graphql.ts
@@ -0,0 +1,94 @@
+/**
+ * @generated SignedSource<<85f26fc928e25f5adee1aa4bdb35943f>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type AppQuery$variables = {};
+export type AppQuery$data = {
+  readonly currentUser: {
+    readonly username: string;
+  } | null;
+};
+export type AppQuery = {
+  response: AppQuery$data;
+  variables: AppQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "username",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "currentUser",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "RootQuery",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "currentUser",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "84b2b5650a0c71cce7c40242b378e52f",
+    "id": null,
+    "metadata": {},
+    "name": "AppQuery",
+    "operationKind": "query",
+    "text": "query AppQuery {\n  currentUser {\n    username\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "fef45d7542db63ccb8b3d59ca5dc6afb";
+
+export default node;
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,46 @@
+// Copyright 2022 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#0DBD8B",
+        alert: "#FF5B55",
+        links: "#0086E6",
+        "grey-25": "#F4F6FA",
+        "grey-50": "#E3E8F0",
+        "grey-100": "#C1C6CD",
+        "grey-150": "#8D97A5",
+        "grey-200": "#737D8C",
+        "grey-250": "#A9B2BC",
+        "grey-300": "#8E99A4",
+        "grey-400": "#6F7882",
+        "grey-450": "#394049",
+        "black-800": "#15191E",
+        "black-900": "#17191C",
+        "black-950": "#21262C",
+        ice: "#F4F9FD",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
