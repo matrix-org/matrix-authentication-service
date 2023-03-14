f8d5a19460af53f637cd98f7f296ae09b3491b92
Quentin Gliech <quenting@element.io>
2023-03-14 10:28:24 +0100

    Fix clippy errors

--- crates/handlers/src/views/register.rs
+++ crates/handlers/src/views/register.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(clippy::trait_duplication_in_bounds)]
-
 use std::{str::FromStr, sync::Arc};
 
 use axum::{
