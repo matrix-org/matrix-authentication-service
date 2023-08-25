f67a6febb81a907d9b557377acb2a8eb5ff5aca2
Kerry Archibald <kerrya@element.io>
2023-08-25 16:31:34 +1200

    better layout test

--- frontend/src/components/Layout.test.tsx
+++ frontend/src/components/Layout.test.tsx
@@ -12,32 +12,68 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { create } from "react-test-renderer";
-import { describe, expect, it, vi, afterAll } from "vitest";
+// @vitest-environment happy-dom
+
+import { render } from "@testing-library/react";
+import { Provider } from "jotai";
+import { useHydrateAtoms } from "jotai/utils";
+import { Suspense } from "react";
+import { describe, expect, it, vi, afterAll, beforeEach } from "vitest";
+
+import { appConfigAtom, locationAtom } from "../Router";
+import { currentUserIdAtom, GqlResult } from "../atoms";
 
 import Layout from "./Layout";
-import NavBar from "./NavBar/NavBar";
 
-const RESULT = Symbol("Result");
-const OK = Symbol("Ok");
-
-vi.mock("jotai", async () => {
-  const actual = await vi.importActual("jotai");
-  return {
-    ...(actual as Record<string, unknown>),
-    useAtomValue: vi
-      .fn()
-      .mockImplementation(() => ({ [RESULT]: OK, [OK]: null })),
-  };
+beforeEach(async () => {
+  // For some reason, the locationAtom gets updated with `about:black` on render,
+  // so we need to set a "real" location and wait for the next tick
+  window.location.assign("https://example.com/");
+  // Wait the next tick for the location to update
+  await new Promise((resolve) => setTimeout(resolve, 0));
 });
 
+const HydrateLocation: React.FC<React.PropsWithChildren<{ path: string }>> = ({
+  children,
+  path,
+}) => {
+  useHydrateAtoms([
+    [appConfigAtom, { root: "/" }],
+    [locationAtom, { pathname: path }],
+  ]);
+  return <>{children}</>;
+};
+
+const WithLocation: React.FC<React.PropsWithChildren<{ path: string }>> = ({
+  children,
+  path,
+}) => {
+  return (
+    <Provider>
+      <Suspense>
+        <HydrateLocation path={path}>{children}</HydrateLocation>
+      </Suspense>
+    </Provider>
+  );
+};
+
 describe("<Layout />", () => {
+  beforeEach(() => {
+    vi.spyOn(currentUserIdAtom, "read").mockResolvedValue(
+      "abc123" as unknown as GqlResult<string | null>,
+    );
+  });
   afterAll(() => {
     vi.restoreAllMocks();
   });
-  it("renders app navigation correctly", () => {
-    const component = create(<Layout />);
+  it("renders app navigation correctly", async () => {
+    const component = render(
+      <WithLocation path="/account">
+        <Layout />
+      </WithLocation>,
+    );
 
-    expect(component.root.findByType(NavBar).props).toMatchSnapshot();
+    expect(await component.findByText("Profile")).toMatchSnapshot();
+    expect(await component.findByText("Home")).toMatchSnapshot();
   });
 });
