6c4205dfad114a636620c7ef0a9d42d3c5c2f7e7
Quentin Gliech <quenting@element.io>
2024-02-06 15:31:49 +0100

    Bump most frontend dependencies & fix CSS nesting warning

--- frontend/src/gql/fragment-masking.ts
+++ frontend/src/gql/fragment-masking.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {
   ResultOf,
   DocumentTypeDecoration,
@@ -8,13 +9,14 @@ import { Incremental } from "./graphql";
 
 export type FragmentType<
   TDocumentType extends DocumentTypeDecoration<any, any>,
-> = TDocumentType extends DocumentTypeDecoration<infer TType, any>
-  ? [TType] extends [{ " $fragmentName"?: infer TKey }]
-    ? TKey extends string
-      ? { " $fragmentRefs"?: { [key in TKey]: TType } }
+> =
+  TDocumentType extends DocumentTypeDecoration<infer TType, any>
+    ? [TType] extends [{ " $fragmentName"?: infer TKey }]
+      ? TKey extends string
+        ? { " $fragmentRefs"?: { [key in TKey]: TType } }
+        : never
       : never
-    : never
-  : never;
+    : never;
 
 // return non-nullable if `fragmentType` is non-nullable
 export function useFragment<TType>(
