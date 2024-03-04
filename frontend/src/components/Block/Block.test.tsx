1ed25f0e1b23c9e96db777732e702e62d40cec94
Michael Telatynski <7t3chguy@gmail.com>
2024-03-04 13:36:51 +0000

    Specify vitest-environment

--- frontend/src/components/Block/Block.test.tsx
+++ frontend/src/components/Block/Block.test.tsx
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// @vitest-environment happy-dom
+
 import { create } from "react-test-renderer";
 import { describe, expect, it } from "vitest";
 
