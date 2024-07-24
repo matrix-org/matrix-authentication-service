43ff6dc9d395707dcd1d20f8f6145901ed057d06
Quentin Gliech <quenting@element.io>
2024-07-24 15:44:08 +0200

    doc: auto-generate the API schema in the documentation

--- misc/update.sh
+++ misc/update.sh
@@ -5,12 +5,14 @@ set -eu
 export SQLX_OFFLINE=1
 BASE_DIR="$(dirname "$0")/.."
 CONFIG_SCHEMA="${BASE_DIR}/docs/config.schema.json"
+API_SCHEMA="${BASE_DIR}/docs/api.schema.json"
 GRAPHQL_SCHEMA="${BASE_DIR}/frontend/schema.graphql"
 POLICIES_SCHEMA="${BASE_DIR}/policies/schema/"
 
 set -x
 cargo run -p mas-config > "${CONFIG_SCHEMA}"
 cargo run -p mas-handlers --bin graphql-schema > "${GRAPHQL_SCHEMA}"
+cargo run -p mas-handlers --bin api-schema > "${API_SCHEMA}"
 cargo run -p mas-i18n-scan -- --update "${BASE_DIR}/templates/" "${BASE_DIR}/translations/en.json"
 OUT_DIR="${POLICIES_SCHEMA}" cargo run -p mas-policy --features jsonschema
 
