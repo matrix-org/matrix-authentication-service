56fcd0d6f179403f43d013b9d103e8941dc16131
Michael Telatynski <7t3chguy@gmail.com>
2024-08-13 12:57:31 +0100

    Redesign cross signing reset unlock flow

--- frontend/src/routes/reset-cross-signing.lazy.tsx
+++ frontend/src/routes/reset-cross-signing.lazy.tsx
@@ -13,9 +13,19 @@
 // limitations under the License.
 
 import { createLazyFileRoute, notFound } from "@tanstack/react-router";
-import IconArrowLeft from "@vector-im/compound-design-tokens/assets/web/icons/arrow-left";
-import IconKey from "@vector-im/compound-design-tokens/assets/web/icons/key";
-import { Alert, Button, Text } from "@vector-im/compound-web";
+import IconCheck from "@vector-im/compound-design-tokens/assets/web/icons/check";
+import IconCheckCircleSolid from "@vector-im/compound-design-tokens/assets/web/icons/check-circle-solid";
+import IconClose from "@vector-im/compound-design-tokens/assets/web/icons/close";
+import IconError from "@vector-im/compound-design-tokens/assets/web/icons/error";
+import IconKeyOffSolid from "@vector-im/compound-design-tokens/assets/web/icons/key-off-solid";
+import { Button, Text } from "@vector-im/compound-web";
+import {
+  ForwardRefExoticComponent,
+  RefAttributes,
+  SVGProps,
+  useState,
+  MouseEvent,
+} from "react";
 import { useTranslation } from "react-i18next";
 import { useMutation, useQuery } from "urql";
 
@@ -24,6 +34,10 @@ import { ButtonLink } from "../components/ButtonLink";
 import Layout from "../components/Layout";
 import LoadingSpinner from "../components/LoadingSpinner";
 import PageHeading from "../components/PageHeading";
+import {
+  VisualList,
+  VisualListItem,
+} from "../components/VisualList/VisualList";
 import { graphql } from "../gql";
 
 import { CURRENT_VIEWER_QUERY } from "./reset-cross-signing";
@@ -51,6 +65,10 @@ export const Route = createLazyFileRoute("/reset-cross-signing")({
   component: ResetCrossSigning,
 });
 
+// This value comes from Synapse and we have no way to query it from here
+// https://github.com/element-hq/synapse/blob/34b758644611721911a223814a7b35d8e14067e6/synapse/rest/admin/users.py#L1335
+const CROSS_SIGNING_REPLACEMENT_PERIOD_MS = 10 * 60 * 1000; // 10 minutes
+
 function ResetCrossSigning(): React.ReactNode {
   const { deepLink } = Route.useSearch();
   const { t } = useTranslation();
@@ -60,6 +78,8 @@ function ResetCrossSigning(): React.ReactNode {
   const userId = viewer.data.viewer.id;
 
   const [result, allowReset] = useMutation(ALLOW_CROSS_SIGING_RESET_MUTATION);
+  const success = !!result.data && !result.error;
+  const error = !success && result.error;
 
   const onClick = async (): Promise<void> => {
     await allowReset({ userId });
@@ -75,53 +95,122 @@ function ResetCrossSigning(): React.ReactNode {
     });
   };
 
+  const [cancelled, setCancelled] = useState(false);
+
+  let cancelButton;
+  if (!deepLink) {
+    cancelButton = (
+      <ButtonLink to="/" kind="tertiary">
+        {t("action.back")}
+      </ButtonLink>
+    );
+  } else if (!success && !error && !cancelled) {
+    // Only show the back button for a deep link if the user hasn't yet completed the interaction
+    cancelButton = (
+      <Button
+        as="a"
+        kind="tertiary"
+        onClick={(ev: MouseEvent) => {
+          ev.preventDefault();
+          setCancelled(true);
+        }}
+      >
+        {t("action.cancel")}
+      </Button>
+    );
+  }
+
+  let Icon: ForwardRefExoticComponent<
+    Omit<SVGProps<SVGSVGElement>, "ref" | "children"> &
+      RefAttributes<SVGSVGElement>
+  >;
+  let title: string;
+  let body: JSX.Element;
+
+  if (cancelled) {
+    Icon = IconKeyOffSolid;
+    title = t("frontend.reset_cross_signing.cancelled.heading");
+    body = (
+      <>
+        <Text className="text-center text-secondary" size="lg">
+          {t("frontend.reset_cross_signing.cancelled.description_1")}
+        </Text>
+        <Text className="text-center text-secondary" size="lg">
+          {t("frontend.reset_cross_signing.cancelled.description_2")}
+        </Text>
+      </>
+    );
+  } else if (success) {
+    Icon = IconCheckCircleSolid;
+    title = t("frontend.reset_cross_signing.success.heading");
+    body = (
+      <Text className="text-center text-secondary" size="lg">
+        {t("frontend.reset_cross_signing.success.description", {
+          minutes: CROSS_SIGNING_REPLACEMENT_PERIOD_MS / (60 * 1000),
+        })}
+      </Text>
+    );
+  } else if (error) {
+    Icon = IconError;
+    title = t("frontend.reset_cross_signing.failure.heading");
+    body = (
+      <Text className="text-center text-secondary" size="lg">
+        {t("frontend.reset_cross_signing.failure.description")}
+      </Text>
+    );
+  } else {
+    Icon = IconError;
+    title = t("frontend.reset_cross_signing.heading");
+    body = (
+      <>
+        <Text className="text-center text-secondary" size="lg">
+          {t("frontend.reset_cross_signing.description")}
+        </Text>
+        <VisualList>
+          <VisualListItem
+            Icon={IconCheck}
+            iconColor="var(--cpd-color-icon-success-primary)"
+            label={t("frontend.reset_cross_signing.effect_list.positive_1")}
+          />
+          <VisualListItem
+            Icon={IconClose}
+            iconColor="var(--cpd-color-icon-critical-primary)"
+            label={t("frontend.reset_cross_signing.effect_list.negative_1")}
+          />
+          <VisualListItem
+            Icon={IconClose}
+            iconColor="var(--cpd-color-icon-critical-primary)"
+            label={t("frontend.reset_cross_signing.effect_list.negative_2")}
+          />
+        </VisualList>
+        <Text className="text-center" size="md" weight="semibold">
+          {t("frontend.reset_cross_signing.warning")}
+        </Text>
+        <Button
+          kind="primary"
+          destructive
+          disabled={result.fetching}
+          onClick={onClick}
+        >
+          {!!result.fetching && <LoadingSpinner inline />}
+          {t("frontend.reset_cross_signing.button")}
+        </Button>
+      </>
+    );
+  }
+
   return (
     <Layout>
       <BlockList>
         <PageHeading
-          Icon={IconKey}
-          title={t("frontend.reset_cross_signing.heading")}
-          invalid
+          Icon={Icon}
+          title={title}
+          invalid={!success}
+          success={success}
         />
 
-        {!result.data && !result.error && (
-          <>
-            <Text className="text-justify">
-              {t("frontend.reset_cross_signing.description")}
-            </Text>
-            <Button
-              kind="primary"
-              destructive
-              disabled={result.fetching}
-              onClick={onClick}
-            >
-              {!!result.fetching && <LoadingSpinner inline />}
-              {t("frontend.reset_cross_signing.button")}
-            </Button>
-          </>
-        )}
-        {result.data && (
-          <Alert
-            type="info"
-            title={t("frontend.reset_cross_signing.success.title")}
-          >
-            {t("frontend.reset_cross_signing.success.description")}
-          </Alert>
-        )}
-        {result.error && (
-          <Alert
-            type="critical"
-            title={t("frontend.reset_cross_signing.failure.title")}
-          >
-            {t("frontend.reset_cross_signing.failure.description")}
-          </Alert>
-        )}
-
-        {!deepLink && (
-          <ButtonLink to="/" kind="tertiary" Icon={IconArrowLeft}>
-            {t("action.back")}
-          </ButtonLink>
-        )}
+        {body}
+        {cancelButton}
       </BlockList>
     </Layout>
   );
